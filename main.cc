291bcde6b91e16752f78312e6f8a543a167b195c
Joseph Sawaya <josephsawaya938@gmail.com>
2022-02-07 13:31:13 -0500

    working

--- main.cc
+++ main.cc
@@ -1,9 +1,20 @@
 #include "main.h"
+#include <iostream>
+#include <fcntl.h>
+#include <sys/ioctl.h>
 
-int main () {
+int main (int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cout << "not enough args" << std::endl;
+        return 1;
+    }
+    int fd = open(argv[1], O_RDWR);
     unsigned int zone_size;
     int ret;
     ret = ioctl(fd, BLKGETZONESZ, &zone_size);
-    if (ret) std::cout << "ERROR\n" << std::endl;
+    if (ret) {
+        std::cout << "ERROR\n" << std::endl;
+        return 1;
+    }
     std::cout << "Size of one zone: " << zone_size << std::endl;
 }
