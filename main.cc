8dc198e795f6bcaf1ab4094996dc3bfda0b8dc02
Joseph Sawaya <josephsawaya938@gmail.com>
2022-02-07 17:17:51 -0500

    working

--- main.cc
+++ main.cc
@@ -1,20 +1,69 @@
 #include "main.h"
-#include <iostream>
-#include <fcntl.h>
-#include <sys/ioctl.h>
+
+void test_write(int fd, blk_zone_report* hdr){
+    int block_size = 4096;
+    const char* alphabet = "abcdefghijklmnopqrstuvwxyz";
+    char buf[block_size];
+    for(int i = 0; i < block_size; i++){
+        buf[i] = alphabet[i % 26];
+    }
+    size_t ret = pwrite(fd, buf, block_size, hdr->zones[0].wp);
+    if (ret != 4096) std::cout << "ERROR WITH TEST_WRITE" << std::endl;
+}
+
+blk_zone_report* get_zone_report(int fd){ 
+    unsigned long long start_sector = 0;
+    blk_zone_report *hdr;
+    size_t hdr_len;
+    int nr_zones;
+    int ret;
+
+    ret = ioctl(fd, BLKGETNRZONES, &nr_zones);
+
+    hdr_len = sizeof(blk_zone_report) + nr_zones * sizeof(blk_zone);
+    hdr = (blk_zone_report* )malloc(hdr_len);
+    if (!hdr) {
+        std::cout << "ERROR MALLOC" << std::endl;
+        return nullptr;
+    }
+
+    while (1) {
+        hdr->sector = start_sector;
+        hdr->nr_zones = nr_zones;
+
+        ret = ioctl(fd, BLKREPORTZONE, hdr);
+
+        if (ret) {
+            std::cout << "ERROR REPORTING ZONE" << std::endl;
+            return nullptr;
+        }
+
+        if(!hdr->nr_zones) {
+            break;
+        }
+
+        start_sector = hdr->zones[hdr->nr_zones-1].start + hdr->zones[hdr->nr_zones -1].len;
+    }
+
+    return hdr;
+}
 
 int main (int argc, char* argv[]) {
     if (argc < 2) {
         std::cout << "not enough args" << std::endl;
         return 1;
     }
+    char* dev_name = argv[1];
     int fd = open(argv[1], O_RDWR);
-    unsigned int zone_size;
+    uint32_t zone_size;
     int ret;
     ret = ioctl(fd, BLKGETZONESZ, &zone_size);
     if (ret) {
-        std::cout << "ERROR\n" << std::endl;
+        std::cout << "ERROR" << std::endl;
         return 1;
     }
     std::cout << "Size of one zone: " << zone_size << std::endl;
+    blk_zone_report* hdr = get_zone_report(fd);
+    test_write(fd, hdr);
 }
+
--- main.h
+++ main.h
@@ -1,1 +1,6 @@
+#pragma once
 #include <linux/blkzoned.h>
+#include <iostream>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
